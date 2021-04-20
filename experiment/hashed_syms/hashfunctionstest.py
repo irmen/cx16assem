635b77626dac8c1b8ca022b42657d941988e8e29
Irmen de Jong <irmen@razorvine.net>
2021-04-20 23:10:11 +0200

    hash

---
+++ experiment/hashed_syms/hashfunctionstest.py
@@ -0,0 +1,57 @@
+import string
+import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def process_word(w):
+    w = w.rstrip()
+    w2 = [char for char in w if char in string.ascii_letters]
+    return "".join(w2).upper()
+
+raw_words = random.sample(open("/usr/share/dict/words", "rt").readlines(), 100)
+words = [pw for pw in [process_word(w) for w in raw_words] if pw]
+
+
+def create_labelname(idx):
+    w1 = random.choice(words)
+    w2 = "" # random.choice(words)
+    name = (w1+w2)[:25].upper()
+    return name + "." + str(idx) + random.choice(string.ascii_uppercase+string.digits)
+
+
+number_of_labels = 5000
+labels = [create_labelname(idx) for idx in range(number_of_labels)]
+
+
+def hashfunc1(label):
+    length = len(label)  # assume we know the length of the symbol already
+    c0 = ord(label[0])
+    c1 = ord(label[1])
+    clast = ord(label[length-1])
+    return ((c0 + clast + c1*4) ^ (length*4)) & 127
+
+
+def hashfunc2(label):
+    # just sum up all the characters of the label....
+    # could be faster / better if you have to scan it anyway to determine the length
+    # also seems to work better for shorter labels
+    length=len(label)
+    return (sum([ord(c) for c in label]) ^ (length*2)) & 127
+
+
+hash_buckets = [0] * 128
+for lbl in labels:
+    hashvalue = hashfunc1(lbl)
+    hash_buckets[hashvalue] += 1
+plt.subplots(figsize = (20,5))
+p=sns.barplot(list(range(len(hash_buckets))), hash_buckets)
+plt.show()
+
+hash_buckets = [0] * 128
+for lbl in labels:
+    hashvalue = hashfunc2(lbl)
+    hash_buckets[hashvalue] += 1
+plt.subplots(figsize = (20,5))
+sns.barplot(list(range(len(hash_buckets))), hash_buckets)
+plt.show()
